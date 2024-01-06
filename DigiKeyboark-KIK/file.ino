34dba8b41f94101ba99425f12bdc1c19d541ce4c
Ayhan G.Y <ayhan.mohammadi.dev@gmail.com>
2024-01-06 10:43:52 -0500

    Create file.ino

---
+++ DigiKeyboark-KIK/file.ino
@@ -0,0 +1,24 @@
+#include "DigiKeyboard.h" 
+//Version‌ -1.2-
+void setup() {  
+  
+} 
+
+void loop() { 
+  DigiKeyboard.delay(2000); 
+  DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT, KEY_R);
+  DigiKeyboard.delay(600); 
+  DigiKeyboard.print(":))))) join t.me/Ayhan_GY 😁"); 
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(5000); 
+  DigiKeyboard.print("http://fakeupdate.net/win10ue"); 
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(2000); 
+  DigiKeyboard.sendKeyStroke(KEY_F11); 
+ 
+//Ring
+ while(true){
+ //&
+} 
+
+}
